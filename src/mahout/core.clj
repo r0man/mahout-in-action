575a21369e1b40a2fde48033fcdfc2b06926da62
Roman <roman@burningswell.com>
2012-02-29 21:29:29 +0100

    Added IFileDataModel.

--- src/mahout/core.clj
+++ src/mahout/core.clj
@@ -1,10 +1,25 @@
 (ns mahout.core
-  (:import [org.apache.mahout.cf.taste.impl.model.file FileDataModel])
-  (:use [clojure.java.io :only (file)]))
+  (:import java.io.File
+           [org.apache.mahout.cf.taste.impl.model.file FileDataModel]))
+
+(defprotocol IFileDataModel
+  (-file-data-model [obj] "Convert `obj` into a FileDataModel."))
 
 (defn file-data-model
   "Returns a FileDataModel from `path`, or nil if it doesn't exists."
-  [path]
-  (if-let [file (file path)]
+  [path] (-file-data-model path))
+
+(extend-protocol IFileDataModel
+  nil
+  (-file-data-model [_]
+    nil)
+  File
+  (-file-data-model [file]
     (if (.exists file)
-      (FileDataModel. file))))
+      (FileDataModel. file)))
+  FileDataModel
+  (-file-data-model [model]
+    model)
+  String
+  (-file-data-model [string]
+    (-file-data-model (File. string))))
