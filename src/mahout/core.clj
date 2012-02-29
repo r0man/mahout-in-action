ad121ae586ad8bca68c96ab60c2dae613961a9d5
Roman <roman@burningswell.com>
2012-02-29 21:43:37 +0100

    Added nearest-n-user-neighborhood.

--- src/mahout/core.clj
+++ src/mahout/core.clj
@@ -1,6 +1,7 @@
 (ns mahout.core
   (:import [org.apache.mahout.cf.taste.impl.model.file FileDataModel]
            [org.apache.mahout.cf.taste.impl.similarity PearsonCorrelationSimilarity]
+           [org.apache.mahout.cf.taste.impl.neighborhood NearestNUserNeighborhood]
            java.io.File))
 
 (defprotocol IFileDataModel
@@ -10,11 +11,18 @@
   "Returns a FileDataModel from `path`, or nil if it doesn't exists."
   [path] (-file-data-model path))
 
+(defn nearest-n-user-neighborhood
+  "Returns a neighborhood computation consisting of the nearest N
+  users to a given user."
+  [n min-similarity user-similarity data-model & [sampling-rate]]
+  (if-let [data-model (file-data-model data-model)]
+    (NearestNUserNeighborhood. n min-similarity user-similarity data-model (or sampling-rate 1))))
+
 (defn pearson-correlation-similarity
-  "Returns the pearson correlation similarity of `model`."
-  [model]
-  (if-let [model (file-data-model model)]
-    (PearsonCorrelationSimilarity. model)))
+  "Returns the pearson correlation similarity of `data-model`."
+  [data-model]
+  (if-let [data-model (file-data-model data-model)]
+    (PearsonCorrelationSimilarity. data-model)))
 
 (extend-protocol IFileDataModel
   nil
@@ -25,8 +33,8 @@
     (if (.exists file)
       (FileDataModel. file)))
   FileDataModel
-  (-file-data-model [model]
-    model)
+  (-file-data-model [data-model]
+    data-model)
   String
   (-file-data-model [string]
     (-file-data-model (File. string))))
--- test/mahout/test/core.clj
+++ test/mahout/test/core.clj
@@ -1,6 +1,7 @@
 (ns mahout.test.core
   (:import [org.apache.mahout.cf.taste.impl.model.file FileDataModel]
-           [org.apache.mahout.cf.taste.impl.similarity PearsonCorrelationSimilarity])
+           [org.apache.mahout.cf.taste.impl.similarity PearsonCorrelationSimilarity]
+           [org.apache.mahout.cf.taste.impl.neighborhood NearestNUserNeighborhood])
   (:use clojure.test
         mahout.core))
 
@@ -10,6 +11,11 @@
   (is (nil? (file-data-model nil)))
   (is (nil? (file-data-model "NOT-EXISTING")))
   (is (instance? FileDataModel (file-data-model intro-dataset-path))))
+
+(deftest test-nearest-n-user-neighborhood
+  (let [user-similarity (pearson-correlation-similarity intro-dataset-path)]
+    (let [neighborhood (nearest-n-user-neighborhood 1 0 user-similarity intro-dataset-path)]
+      (is (instance? NearestNUserNeighborhood neighborhood)))))
 
 (deftest test-pearson-correlation-similarity
   (is (nil? (pearson-correlation-similarity nil)))
